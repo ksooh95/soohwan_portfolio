d8737ffd11abee3723a0cfcdc68757bf0c15e261
KIMSOOHWAN <ksooh95@gmail.com>
2024-06-18 19:57:16 +0900

    fix : vercel 배포 위한 vercel.json 파일 추가

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import PortModal from './components/port_modal';
-
+x``
 export default function Home() {
     const [modal, setModal] = useState('');
     const closeModal = () => {

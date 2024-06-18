d0b4f6590d4ecb997ac6071cb988cee7303e7a4d
KIMSOOHWAN <ksooh95@gmail.com>
2024-06-18 19:41:29 +0900

    feat : 포트폴리오 프로젝트 부분 제작

--- app/components/port_modal.jsx
+++ app/components/port_modal.jsx
@@ -1,22 +1,89 @@
-import React from 'react';
+'use client';
 
-const PortModal = ({ closeModal }) => {
+import React, { useState } from 'react';
+
+const PortModal = ({ closeModal, project }) => {
+    const [largeImage, setLargeImage] = useState(null);
+
+    const handleImageClick = (imgUrl) => {
+        setLargeImage(imgUrl);
+    };
+
+    const closeLargeImageModal = () => {
+        setLargeImage(null);
+    };
     return (
         <div className="modal">
-            <div className="modal_header">
-                <h1>SooHwan Portfolio</h1>
-                <button
-                    onClick={() => {
-                        closeModal('');
-                    }}
-                >
-                    <img src="/close.png" alt="" />
-                </button>
+            <div className="modal_back"></div>
+            <div className="modal_body">
+                <div className="modal_header">
+                    <h1>{project.title}</h1>
+                    <button
+                        onClick={() => {
+                            closeModal('');
+                        }}
+                    >
+                        <img src="/close.png" alt="" />
+                    </button>
+                </div>
+                <div className="modal_content">
+                    <div className="l_con">
+                        <img src={project.img} alt="" />
+                    </div>
+                    <div className="r_con">
+                        <p>{project.description2}</p>
+                        <ul>
+                            <li>
+                                <span>Stacks : {project.stack.join(', ')}</span>
+                            </li>
+                            <li>
+                                <span>Deployment : Vercel</span>
+                            </li>
+                            <li>
+                                <span>
+                                    배포 주소 :{' '}
+                                    <a href={project.deploy === null ? '/' : project.deploy}>
+                                        {project.deploy === null ? '배포중단' : project.deploy}
+                                    </a>{' '}
+                                </span>
+                            </li>
+                            <li>
+                                <span>
+                                    깃허브 :{' '}
+                                    <a href={project.git === null ? '/' : project.git} target="_blank">
+                                        {project.git === null ? '-' : project.git}
+                                    </a>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="more_img">
+                    {project.subimg
+                        ? project.subimg.map((subimg, i) => {
+                              return (
+                                  <div className="subimg">
+                                      <div className="subimg_hover" onClick={() => handleImageClick(subimg)}>
+                                          <img src="/scale2.png" alt="" />
+                                      </div>
+                                      <img src={subimg} alt="" key={i} />
+                                  </div>
+                              );
+                          })
+                        : ''}
+                </div>
             </div>
-            <div className="modal_content">
-                <div className="l_con"></div>
-                <div className="r_con"></div>
-            </div>
+            {largeImage && (
+                <div className="large_image_modal">
+                    <div className="large_image_back" onClick={closeLargeImageModal}></div>
+                    <div className="large_image_body">
+                        <img src={largeImage} alt="Large" />
+                        <button onClick={closeLargeImageModal}>
+                            <img src="/close.png" alt="Close" />
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- app/page.js
+++ app/page.js
@@ -12,56 +12,76 @@ export default function Home() {
     const [modal, setModal] = useState('');
     const closeModal = () => {
         setModal('');
+        document.body.style.overflow = 'auto';
     };
 
     const project = [
         {
             tag: '개인 프로젝트',
             img: '/port.png',
+            subimg: ['/port2.png', '/port3.png'],
             title: 'SooHwan Portfolio',
             description:
+                '김수환의 개인 포트폴리오 사이트, 개인프로젝트와 팀프로젝트 등 한데 모아둔 포트폴리오 사이트입니다.',
+            description2:
                 '김수환의 개인 포트폴리오 사이트, 개인프로젝트와 팀프로젝트 등 한데 모아둔 포트폴리오 사이트입니다.',
             status: ['배포예정', '업데이트중', '개인 프로젝트'],
             git: 'https://github.com/ksooh95/soohwan_portfolio',
             deploy: null,
+            stack: ['NextJS', 'React', 'HTML', 'CSS', 'GIT'],
         },
         {
             tag: '개인 프로젝트',
             img: '/smdb.png',
+            subimg: ['/smdb2.png', '/smdb3.png', '/smdb4.png'],
             title: 'SMDB MOVIE',
             description:
                 'NextJS 환경에서 혼자 만들어본 TMDB OPEN API 를 사용하여 현재 개봉영화, 인기있는 TV시리즈, 현재 검색 트렌드 등을 소개해주는 사이트입니다.',
+            description2:
+                'NextJS 환경에서 혼자 만들어본 TMDB OPEN API를 사용하여 현재 개봉 영화, 인기 있는 TV 시리즈, 현재 검색 트렌드 등을 소개해주는 사이트입니다.\n\n상세 페이지에 들어가면 영화 혹은 TV 시리즈의 장르, 시간 등 상세 정보를 얻을 수 있고, 트레일러 영상 및 줄거리도 확인할 수 있습니다.\n\n 게시판을 만들어 서로 영화에대한 이야기를 할수있는 공간을 만들었고 \n\n처음으로 무한 스크롤 및 스켈레톤 UI를 적용하여 사용자의 경험을 최대한 좋게 하려고 노력하였습니다.',
             status: ['배포완료', '업데이트중', '개인 프로젝트'],
             git: 'https://github.com/ksooh95/SMDB',
             deploy: 'https://vercel-smdb.vercel.app/',
+            stack: ['NextJS', 'NextAuth', 'React', 'MongoDB', 'HTML', 'CSS', 'GIT'],
         },
         {
             tag: '팀 프로젝트',
-            img: '/pop.png',
+            img: '/popgif.gif',
+            subimg: ['/pop5.png', '/pop3.png', '/pop4.png'],
             title: 'Pop, spot !',
             description:
                 '엘리스 부트캠프 2차 팀프로젝트때 팀원들과 제작했던 팝업스토어의 정보와 예약을 할수있는 홈페이지 입니다.',
+            description2:
+                '엘리스 부트캠프 2차 팀프로젝트때 팀원들과 제작했던 팝업스토어의 정보와 예약을 할수있는 홈페이지 입니다. \n\n 제가 담당했던 페이지는 상세페이지, 리뷰작성 및 확인, 예약기능 부분을 담당하였습니다. \n 프론트와 백 둘다 같이 작업하였습니다. \n\n 배포가 중단되어 작업했던 페이지를 gif로 넣었습니다.',
             status: ['배포중단', '제작완료', '팀 프로젝트'],
-            git: '',
+            git: 'https://github.com/ksooh95/pop.spot',
             deploy: null,
+            stack: ['React', 'MongoDB', 'NodeJS', 'HTML', 'CSS', 'GIT'],
         },
         {
             tag: '개인 프로젝트',
             img: '/memo.png',
+            subimg: ['/memo2.png', '/memo3.png'],
             title: 'MEMO APP',
             description: 'NextJs 환경에서 MongoDB를 사용하여 프론트와,백 둘다 처음 혼자 만들어본 간단한 메모 앱입니다.',
+            description2:
+                'NextJs 환경에서 MongoDB를 사용하여 프론트와,백 둘다 처음 혼자 만들어본 간단한 메모 앱입니다.',
             status: ['배포완료', '업데이트중', '개인 프로젝트'],
             git: 'https://github.com/ksooh95/memoapp',
             deploy: 'https://memoapp-sable.vercel.app/list',
+            stack: ['NextJS', 'React', 'MongoDB', 'HTML', 'CSS', 'GIT'],
         },
         {
             tag: '팀 프로젝트',
             img: '/panda.png',
+            subimg: ['/pop5.png', '/pop3.png', '/pop4.png'],
             title: 'Panda Flower',
             description: '엘리스 부트캠프 팀프로젝트 1차 때 만든 카테고리별로 꽃을 판매하는 사이트를 제작하였습니다.',
+            description2: '엘리스 부트캠프 팀프로젝트 1차 때 만든 카테고리별로 꽃을 판매하는 사이트를 제작하였습니다.',
             status: ['배포중단', '제작완료', '팀 프로젝트'],
             git: '',
             deploy: null,
+            stack: ['MongoDB', 'HTML', 'CSS', 'GIT'],
         },
     ];
 
@@ -86,7 +106,7 @@ export default function Home() {
                     <span className="ar_t">SCROLL</span>
                 </div>
                 <div className="update">
-                    <p>Update 2024.06.15</p>
+                    <p>Update 2024.06.18</p>
                 </div>
             </div>
             <div className="con2">
@@ -159,7 +179,7 @@ export default function Home() {
                                                         className="more_btn"
                                                         onClick={() => {
                                                             setModal(a.title);
-                                                            console.log(modal);
+                                                            document.body.style.overflow = 'hidden';
                                                         }}
                                                     >
                                                         자세히보기
@@ -193,7 +213,17 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            {modal === '' ? null : <PortModal closeModal={closeModal} />}
+            {modal === 'SooHwan Portfolio' ? (
+                <PortModal closeModal={closeModal} project={project[0]} />
+            ) : modal === 'SMDB MOVIE' ? (
+                <PortModal closeModal={closeModal} project={project[1]} />
+            ) : modal === 'Pop, spot !' ? (
+                <PortModal closeModal={closeModal} project={project[2]} />
+            ) : modal === 'MEMO APP' ? (
+                <PortModal closeModal={closeModal} project={project[3]} />
+            ) : modal === 'Panda Flower' ? (
+                <PortModal closeModal={closeModal} project={project[4]} />
+            ) : null}
         </div>
     );
 }
